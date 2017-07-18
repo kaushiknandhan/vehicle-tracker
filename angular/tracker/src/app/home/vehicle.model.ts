5b1c7f1884a35631b8608046efb7a23ffab73b82
kaushik nandhan <kaushiknandhan@yahoo.com>
2017-07-18 09:22:02 -0700

    feat(module-angular): add vehicle model

---
+++ angular/tracker/src/app/home/vehicle.model.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by kaushik nandhan on 7/16/2017.
+ */
+
+export interface Vehicle{
+  vin:string;
+  make:string;
+  model:string;
+  year:number;
+  redlineRpm:number;
+  maxFuelVolume:number;
+  lastServiceDate:Date;
+}
